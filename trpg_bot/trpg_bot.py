3449ac41144d0dcf800a268f775e0d740b392476
k-seta <swordh16@gmail.com>
2020-04-25 16:33:10 +0900

    ndnの書式判定を追加

--- trpg_bot/trpg_bot.py
+++ trpg_bot/trpg_bot.py
@@ -2,7 +2,12 @@
 #-*- coding:utf-8 -*-
 
 import os
+import re
 import discord
+
+def validate_ndn(message):
+    pattern = '/(\d+?)d(\d+?)'
+    return re.match(pattern, message)
 
 if __name__ == '__main__':
     TOKEN = os.environ['DISCORD_BOT_TOKEN']
@@ -18,5 +23,9 @@ if __name__ == '__main__':
             return
         if message.content == '/ping':
             await message.channel.send('pong')
+
+        match_obj = validate_ndn(message.content)
+        if match_obj:
+            await message.channel.send('dice')      
 
     client.run(TOKEN)
