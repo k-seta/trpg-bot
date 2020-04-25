ca3ade30b1fada89457557809b313d57994b3ece
k-seta <swordh16@gmail.com>
2020-04-25 16:14:46 +0900

    if __name__==__main__を追加

--- trpg_bot/trpg_bot.py
+++ trpg_bot/trpg_bot.py
@@ -4,18 +4,19 @@
 import os
 import discord
 
-TOKEN = os.environ['DISCORD_BOT_TOKEN']
-client = discord.Client()
+if __name__ == '__main__':
+    TOKEN = os.environ['DISCORD_BOT_TOKEN']
+    client = discord.Client()
 
-@client.event
-async def on_ready():
-    print('activated lavoce-trpg-bot client.')
+    @client.event
+    async def on_ready():
+        print('activated trpg-bot client.')
 
-@client.event
-async def on_message(message):
-    if message.author.bot:
-        return
-    if message.content == '/ping':
-        await message.channel.send('pong')
+    @client.event
+    async def on_message(message):
+        if message.author.bot:
+            return
+        if message.content == '/ping':
+            await message.channel.send('pong')
 
-client.run(TOKEN)
+    client.run(TOKEN)
