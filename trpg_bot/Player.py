0e850c72c6820324b386d61878857929971f7266
k-seta <swordh16@gmail.com>
2020-04-27 22:04:53 +0900

    poetryの依存関係からdiscord.pyのvoiceを削除

---
+++ trpg_bot/Player.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+
+class Player:
+
+    user = 'user'
+    url = 'url'
+
+    def __init__(user, url, self):
+        self.user = user
+        self.url = url
+
+    def get_status(self):
+        
