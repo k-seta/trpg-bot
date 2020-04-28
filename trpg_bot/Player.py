0fcf37e53dbd1ba56a65a0c07c90f77970837a9a
k-seta <swordh16@gmail.com>
2020-04-28 22:56:45 +0900

    beautifulsoupを用いて技能を取得する機能の実装

--- trpg_bot/Player.py
+++ trpg_bot/Player.py
@@ -30,6 +30,12 @@ class Player:
     INT = '0' 
     EDU = '0'
 
+    battle_arts = {}
+    find_arts = {}
+    act_arts = {}
+    commu_arts = {}
+    know_arts = {}
+
     def __init__(self, user, url):
         self.user = user
         self.url = url
@@ -56,6 +62,17 @@ class Player:
         self.INT = soup.find('input', {'name': 'NP7'})['value']
         self.EDU = soup.find('input', {'name': 'NP8'})['value']
 
+        self.battle_arts = self.extract_table(soup, 'Table_battle_arts', 'TBAP[]')
+        self.find_arts = self.extract_table(soup, 'Table_find_arts', 'TFAP[]')
+        self.act_arts = self.extract_table(soup, 'Table_act_arts', 'TAAP[]')
+        self.commu_arts = self.extract_table(soup, 'Table_commu_arts', 'TCAP[]')
+        self.know_arts = self.extract_table(soup, 'Table_know_arts', 'TKAP[]')
+
+    def extract_table(self, soup, table_id, value_name):
+        keys = [key.text if key.text != '' else 'その他' for key in soup.find('table', {'id': table_id}).find_all('th',{})[8:]]
+        values = [value['value'] for value in soup.find('table', {'id': table_id}).find_all('input', {'name': value_name})]
+        return dict(zip(keys, values))
+
     def print(self):
         table = PrettyTable()
         table.field_names = ['HP', 'MP', 'IDA', 'LUK', 'ACK']
@@ -67,9 +84,41 @@ class Player:
         table.add_row([self.STR, self.CON, self.POW, self.DEX, self.APP, self.SIZ, self.INT, self.EDU])
         basic_status = table.get_string()
 
-        return (f"{self.name}\n"
-                f"SAN値: {self.SAN_LEFT}/{self.SAN_MAX}\n"
+        battle_status = ''
+        for key, value in self.battle_arts.items():
+            battle_status += f"{key}: {value}\n"
+
+        find_status = ''
+        for key, value in self.find_arts.items():
+            find_status += f"{key}: {value}\n"
+
+        act_status = ''
+        for key, value in self.act_arts.items():
+            act_status += f"{key}: {value}\n"
+
+        commu_status = ''
+        for key, value in self.commu_arts.items():
+            commu_status += f"{key}: {value}\n"
+
+        know_status = ''
+        for key, value in self.know_arts.items():
+            know_status += f"{key}: {value}\n"
+
+        return (f"{self.name}\n\n"
+                "【能力値】\n"
+                f"SAN値: {self.SAN_LEFT}/{self.SAN_MAX}"
                 '\n'
                 f"{mutable_status}\n"
                 '\n'
-                f"{basic_status}\n")
+                f"{basic_status}\n"
+                '\n'
+                '【戦闘技能】\n'
+                f"{battle_status}\n"
+                '【探索技能】\n'
+                f"{find_status}\n"
+                '【行動技能】\n'
+                f"{act_status}\n"
+                '【交渉技能】\n'
+                f"{commu_status}\n"
+                '【知識技能】\n'
+                f"{know_status}\n")
