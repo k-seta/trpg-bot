3ccd4aa5e222355a4664cb71f6034be90c5491fd
k-seta <swordh16@gmail.com>
2020-05-10 18:54:22 +0900

    #1 /modeコマンドを作成

--- trpg_bot/trpg_bot.py
+++ trpg_bot/trpg_bot.py
@@ -3,26 +3,15 @@
 
 import os
 import re
-import random
-import itertools
 import traceback
 import discord
-import json
 import redis
-from prettytable import PrettyTable
 
-from player.CthulhuPlayer import CthulhuPlayer
+from logic.ModeSelectorLogic import ModeSelectorLogic
 
-def dice_ndn(message_ndn):
-    pattern_dice = '(\d+)d(\d+)'
-    match_dice = re.search(pattern_dice, message_ndn)
-    if not match_dice:
-        pattern_const = '(\d+)'
-        match_const = re.search(pattern_const, message_ndn)
-        return [int(match_const.group(1))] if match_const else []
-    quantity = int(match_dice.group(1))
-    size = int(match_dice.group(2))
-    return [random.randint(1, size) for i in range(quantity)]
+def validate_mode(message):
+    pattern = '^/mode (.*)'
+    return re.match(pattern, message)
 
 def validate_regist(message):
     pattern = '^/regist (http.*)'
@@ -35,15 +24,12 @@ def validate_ndn(message):
 if __name__ == '__main__':
     TOKEN = os.environ['DISCORD_BOT_TOKEN']
     REDIS = os.environ['REDIS_URL']
-
     GLOBAL_CHANNEL_ID = int(os.environ['GLOBAL_CHANNEL_ID'])
 
     client = discord.Client()
     r = redis.from_url(os.environ.get("REDIS_URL"), decode_responses=True)
 
-    reply_help = ''
-    with open('./trpg_bot/usage.md', 'r') as f:
-        reply_help = f.read()
+    mode_selector = ModeSelectorLogic(r)
 
     @client.event
     async def on_ready():
@@ -65,50 +51,33 @@ if __name__ == '__main__':
                 print(reply)
                 await message.channel.send(f"```{reply}```")
 
+            match_mode = validate_mode(message.content)
+            if match_mode:
+                key = match_mode.group(1)
+                mode_selector.select(key)
+                await message.channel.send(f"{key} モードになったよ")
+
             if message.content == '/help':
-                await message.channel.send(reply_help)
+                reply = mode_selector.get().help()
+                await message.channel.send(reply)
 
             match_regist = validate_regist(message.content)
             if match_regist:
-                session = message.channel.name
-                user = message.author.name
                 url = match_regist.group(1)
-                r.hset(session, user, url)
-                reply = f"{message.author.mention} がキャラシートを登録したよ\n=> {url}"
-                await message.channel.send(reply)
+                mode_selector.get().regist(message, url)
+                await message.channel.send(f"{message.author.mention} がキャラシートを登録したよ\n=> {url}")
 
             if message.content == '/players':
-                session = message.channel.name
-                data = r.hgetall(session)
-                table = PrettyTable()
-                table.field_names = ['user', 'url']
-                for user, url in data.items():
-                    table.add_row([user, url])
-                await message.channel.send(f"{message.channel.mention} のキャラシート一覧だよ\n```{table.get_string()}```")
+                table = mode_selector.get().players(message)
+                await message.channel.send(f"{message.channel.mention} のキャラシート一覧だよ\n```{table}```")
 
             if validate_ndn(message.content):
-                elements = message.content.split('+')
-                dices = [dice_ndn(e) for e in elements]
-                sum_dices = sum(list(itertools.chain.from_iterable(dices)))
-                reply = f"{message.author.mention} がサイコロを振ったよ\n=> {sum_dices}    {str(dices)[1:-1]}"
-                
-                if '<' in message.content:
-                    session = message.channel.name
-                    user = message.author.name
-                    url = r.hget(session, user)
-                    player = CthulhuPlayer(user, url)
-
-                    param_key = message.content.split('<')[1].strip()
-                    param_value = player.get(param_key)
-                    reply += f" < {param_key}({param_value})"
+                result = mode_selector.get().dice(message)
+                reply = f"{message.author.mention} がサイコロを振ったよ\n=> {result}"
                 await message.channel.send(reply)
 
             if message.content == '/status':
-                session = message.channel.name
-                user = message.author.name
-                url = r.hget(session, user)
-                player = CthulhuPlayer(user, url)
-                status = player.print()
+                status = mode_selector.get().status(message)
                 await message.channel.send(f"{message.author.mention} のキャラシートだよ\n```{status}```")
 
         except Exception as e:
