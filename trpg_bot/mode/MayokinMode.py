05cf56b402b6d4522385280c419df2de59a9393a
k-seta <swordh16@gmail.com>
2020-05-16 15:39:56 +0900

    /d66の機能をMayokinModeに追加

--- trpg_bot/mode/MayokinMode.py
+++ trpg_bot/mode/MayokinMode.py
@@ -1,9 +1,23 @@
 #!/usr/bin/env python
 #-*- coding:utf-8 -*-
 
+import re
+
 from mode.DefaultMode import DefaultMode
+from logic.DiceListLogic import DiceListLogic
+from logic.DiceLogic import DiceLogic
 
 class MayokinMode(DefaultMode):
 
     def __init__(self, redis, path_of_help_md):
         super().__init__(redis, path_of_help_md)
+
+    def dice(self, message):
+        match_d66 = re.search('/d66(.*)', message.content)
+        print (match_d66)
+        if match_d66:
+            name = match_d66.group(1).strip()
+            path = f"./trpg_bot/resources/mayokin/{name}.txt"
+            value = DiceLogic.roll_d66()
+            return DiceListLogic.disp(path, value) if len(name) != 0 else value
+        return super().dice(message)
