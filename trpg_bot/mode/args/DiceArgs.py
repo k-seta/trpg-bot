a944376f75b8a71421692376766ba06c80663de0
k-seta <swordh16@gmail.com>
2020-05-22 22:53:56 +0900

    DiceArgsの不具合を修正

--- trpg_bot/mode/args/DiceArgs.py
+++ trpg_bot/mode/args/DiceArgs.py
@@ -9,21 +9,22 @@ class DiceArgs:
             raise Exception('Type error: value')
         
         if type(items) == list:
-            self.items = items
-        elif type(items) == int:
             self.items = [items]
+        elif type(items) == int:
+            self.items = [[items]]
         else:
             raise Exception('Type error: items')
 
     def __add__(self, other):
         if type(other) == DiceArgs:
             self.value += other.value
-            self.items.append(other.items)
+            self.items += other.items
         elif type(other) == int:
             self.value += other
-            self.items.append([other])
+            self.items += [[other]]
         else:
             raise Exception('Type error: + operator')
+        return self
 
     def __str__(self):
         return f"{self.value}  {str(self.items)[1:-1]}"
