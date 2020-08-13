2bc6e8613436118f2ddef2e2da8ae35b7ce54ef7
k-seta <swordh16@gmail.com>
2020-08-13 21:07:24 +0900

    DefaultMode.extraを作成

--- trpg_bot/mode/DefaultMode.py
+++ trpg_bot/mode/DefaultMode.py
@@ -66,3 +66,6 @@ class DefaultMode:
             else:
                 result_values.append(head)
         return ' '.join([str(value) for value in result_values])
+
+    def extra(self, param):
+        return '該当コマンドが無いよ\n/help で使えるコマンドを確認できるよ'
