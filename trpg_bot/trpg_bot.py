66988da7b0503cbab377cefbaa83eea8eee5508b
k-seta <swordh16@gmail.com>
2021-05-06 22:39:44 +0900

    channel削除時のredisテーブルのキーを変更

--- trpg_bot/trpg_bot.py
+++ trpg_bot/trpg_bot.py
@@ -91,7 +91,7 @@ if __name__ == '__main__':
     @client.event
     async def on_guild_channel_delete(channel):
         try:
-            r.hdel('mode', channel.name)
+            r.hdel(f"{channel.guild.name}.mode", channel.name)
             r.delete(channel.name)
         except Exception as e:
             traceback.print_exc()
